4063af24bf6ac131939f2e51c1c83f24c8869d10
Qiang <qzeng2490@yahoo.com>
2014-02-21 15:58:58 -0500

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TCP.rc
+//
+#define IDC_MYICON                      2
+#define IDD_TCP_DIALOG                  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_TCP                         107
+#define IDI_SMALL                       108
+#define IDC_TCP                         109
+#define IDR_MAINFRAME                   128
+#define IDM_INPUT                       130
+#define ID_IPEDIT                       3000
+#define ID_PORTEDIT                     3001
+#define ID_CONNBTN                      3002
+#define ID_LISTENBTN                    3003
+#define ID_ACCELERATOR32775             32775
+#define IDC_ACCEL                       32775
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
